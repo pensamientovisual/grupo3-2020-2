fb5c9e5a5bb0d083e4b2464cd7ea104ad44162b1
Josefina Vial <josevial@uc.cl>
2020-10-13 15:17:05 -0300

    regiones

--- archivojava.js
+++ archivojava.js
@@ -1,8 +1,69 @@
 $(document).ready(function(){
+    var aisen = $("#CL-AI");
+    aisen.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    aisen.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var magallanes = $("#CL-MA");
+    magallanes.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    magallanes.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var antofagasta = $("#CL-AN");
+    antofagasta.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    antofagasta.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var atacama = $("#CL-AT");
+    atacama.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    atacama.mouseleave(function(){$(this).fadeTo("fast",1);});
+    
+    var coquimbo = $("#CL-CO");
+    coquimbo.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    coquimbo.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var valparaiso = $("#CL-VS");
+    valparaiso.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    valparaiso.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var araucania = $("#CL-AR");
+    araucania.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    araucania.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var libertador = $("#CL-LI");
+    libertador.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    libertador.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var lagos = $("#CL-LL");
+    lagos.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    lagos.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var maule = $("#CL-ML");
+    maule.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    maule.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var arica = $("#CL-AP");
+    arica.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    arica.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var rios = $("#CL-LR");
+    rios.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    rios.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var tarapaca = $("#CL-TA");
+    tarapaca.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    tarapaca.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var biobio = $("#Cl-BI");
+    biobio.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    biobio.mouseleave(function(){$(this).fadeTo("fast",1);});
+
+    var nuble = $("#CL-NU");
+    nuble.mouseenter(function(){$(this).fadeTo("fast",0.5);});
+    nuble.mouseleave(function(){$(this).fadeTo("fast",1);});
+
     var santiago = $("#CL-RM");
     santiago.mouseenter(function(){$(this).fadeTo("fast",0.5);});
     santiago.mouseleave(function(){$(this).fadeTo("fast",1);});
     santiago.click(function()
     {$("#foto").toggle()
     ;})
+
 });
